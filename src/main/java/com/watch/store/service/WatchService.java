c1750a2d342fcedef497b8992da95bb2ba2f5c67
Teo Ciubuc <ciubucteo@yahoo.com>
2023-07-03 20:51:23 +0300

    Create watches

--- src/main/java/com/watch/store/service/WatchService.java
+++ src/main/java/com/watch/store/service/WatchService.java
@@ -1,18 +1,29 @@
 package com.watch.store.service;
 
+import com.watch.store.dto.WatchDto;
 import com.watch.store.entity.Watch;
+import com.watch.store.mapper.WatchMapper;
 import com.watch.store.repository.WatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class WatchService {
     @Autowired
     WatchRepository watchRepository;
+    @Autowired
+    private WatchMapper watchMapper;
 
     public List<Watch> listAllWatches(){
         return watchRepository.findAll();
+    }
+
+    public void saveWatch(WatchDto watchDto, MultipartFile file) throws IOException {
+    Watch watch = watchMapper.watchMapper(watchDto,file);
+    watchRepository.save(watch);
     }
 }
